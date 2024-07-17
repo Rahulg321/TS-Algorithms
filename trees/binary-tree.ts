b20cb5432b5d8e645bc5f7493abb41a82f6208e3
Rahul Gupta <rg5353070@gmail.com>
2024-07-17 14:14:10 +0530

    added methods for traversal and search in tree

--- trees/binary-tree.ts
+++ trees/binary-tree.ts
@@ -1,2 +1,165 @@
-class BinaryTreeNode {}
-class BinaryTree {}
+/* 
+    in a binary tree, each node has exactly two child Nodes, nothing else
+*/
+
+class BinaryTreeNode {
+  val: number;
+  left: BinaryTreeNode | null;
+  right: BinaryTreeNode | null;
+
+  constructor(
+    value: number,
+    leftNode?: BinaryTreeNode,
+    rightNode?: BinaryTreeNode
+  ) {
+    this.val = value;
+    // if the left and right node exists then assign value to it
+    this.left = leftNode === undefined ? null : leftNode;
+    this.right = rightNode === undefined ? null : rightNode;
+  }
+}
+class BinaryTree {
+  rootNode: BinaryTreeNode | null;
+
+  constructor() {
+    this.rootNode = null;
+  }
+
+  insert(value: number | null) {
+    if (!value) {
+      return;
+    }
+
+    const newNode = new BinaryTreeNode(value);
+
+    if (!this.rootNode) {
+      this.rootNode = newNode;
+    } else {
+      this.insertNode(this.rootNode, newNode);
+    }
+  }
+
+  insertNode(node: BinaryTreeNode, newNode: BinaryTreeNode) {
+    // this insert function maintains order of elements in binary tree
+    // since we are maintaining order this type of tree is known as binary search tree
+
+    if (newNode.val < node.val) {
+      if (node.left === null) {
+        console.log("inserted node at the left");
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        console.log("inserted node at the right");
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  preOrderTraversal(rootNode: BinaryTreeNode | null) {
+    // root -> left -> right
+    if (!rootNode) {
+      return null;
+    }
+
+    console.log(rootNode.val);
+    this.preOrderTraversal(rootNode.left);
+    this.preOrderTraversal(rootNode.right);
+  }
+
+  inOrderTraversal(rootNode: BinaryTreeNode | null) {
+    // root -> left -> right
+    if (!rootNode) {
+      return null;
+    }
+
+    this.inOrderTraversal(rootNode.left);
+    console.log(rootNode.val);
+    this.inOrderTraversal(rootNode.right);
+  }
+
+  levelOrderTraversal(): void {
+    if (!this.rootNode) {
+      // there is no tree and no node in the tree
+      return;
+    }
+
+    let queue: (BinaryTreeNode | null)[] = [this.rootNode];
+
+    while (queue.length > 0) {
+      // return the first element from the queue
+      let node = queue.shift();
+
+      if (node) {
+        console.log(node.val);
+        if (node.left) {
+          queue.push(node.left);
+        }
+
+        if (node.right) {
+          queue.push(node.right);
+        }
+      }
+    }
+  }
+
+  postOrderTraversal(rootNode: BinaryTreeNode | null) {
+    // root -> left -> right
+    if (!rootNode) {
+      return null;
+    }
+
+    this.postOrderTraversal(rootNode.left);
+    this.postOrderTraversal(rootNode.right);
+    console.log(rootNode.val);
+  }
+
+  search(value: number): BinaryTreeNode | null {
+    return this.searchNode(this.rootNode, value);
+  }
+
+  searchNode(
+    node: BinaryTreeNode | null,
+    value: number
+  ): BinaryTreeNode | null {
+    if (!node) {
+      console.log("the tree is empty, root node does not exist");
+      return null;
+    }
+
+    if (value < node.val) {
+      return this.searchNode(node.left, value);
+    } else if (value > node.val) {
+      return this.searchNode(node.right, value);
+    } else {
+      return node;
+    }
+  }
+}
+
+let BT = new BinaryTree();
+
+BT.insert(50);
+BT.insert(30);
+BT.insert(70);
+BT.insert(20);
+BT.insert(40);
+BT.insert(10);
+BT.insert(25);
+BT.insert(27);
+BT.insert(60);
+BT.insert(80);
+BT.insert(55);
+
+// console.log("postorder traversal is");
+// BT.postOrderTraversal(BT.rootNode);
+
+const result = BT.search(27);
+console.log("result is ", result);
+if (!result) {
+  console.log("could not find the node");
+}
--- trees/main.ts
+++ trees/main.ts
@@ -1,46 +1,46 @@
-class TreeNode<T> {
-  value: T;
-  children: TreeNode<T>[] = [];
+// class TreeNode<T> {
+//   value: T;
+//   children: TreeNode<T>[] = [];
 
-  constructor(val: T) {
-    this.value = val;
-    this.children = [];
-  }
+//   constructor(val: T) {
+//     this.value = val;
+//     this.children = [];
+//   }
 
-  addChild(node: TreeNode<T>) {
-    this.children.push(node);
-  }
+//   addChild(node: TreeNode<T>) {
+//     this.children.push(node);
+//   }
 
-  removeChild(node: TreeNode<T>) {
-    this.children.filter((child) => child !== node);
-  }
-}
+//   removeChild(node: TreeNode<T>) {
+//     this.children.filter((child) => child !== node);
+//   }
+// }
 
-class Tree<T> {
-  rootNode: TreeNode<T> | null;
+// class Tree<T> {
+//   rootNode: TreeNode<T> | null;
 
-  constructor(rootNodeData: T) {
-    // initialize with a new root Node
-    this.rootNode = new TreeNode(rootNodeData);
-  }
+//   constructor(rootNodeData: T) {
+//     // initialize with a new root Node
+//     this.rootNode = new TreeNode(rootNodeData);
+//   }
 
-  breadthFirstSearch() {}
-  depthFirstSearch() {}
+//   breadthFirstSearch() {}
+//   depthFirstSearch() {}
 
-  inOrderTraversal() {}
-  preOrderTraversal() {}
-  postOrderTraversal() {}
-}
+//   inOrderTraversal() {}
+//   preOrderTraversal() {}
+//   postOrderTraversal() {}
+// }
 
-// creating nodes for a tree
-let rootNode = new TreeNode(1);
-let two = new TreeNode(2);
-let three = new TreeNode(3);
+// // creating nodes for a tree
+// let rootNode = new TreeNode(1);
+// let two = new TreeNode(2);
+// let three = new TreeNode(3);
 
-// add a root node for the tree
+// // add a root node for the tree
 
-// create a new tree
-let simpleTree = new Tree(rootNode);
+// // create a new tree
+// let simpleTree = new Tree(rootNode);
 
-rootNode.addChild(two);
-rootNode.addChild(three);
+// rootNode.addChild(two);
+// rootNode.addChild(three);
