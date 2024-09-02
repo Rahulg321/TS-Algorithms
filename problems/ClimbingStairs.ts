99bc78dce78103b140fd07395c3e72dfcfa83f26
Shivam Chopra <parthasus7973@gmail.com>
2024-09-02 19:18:05 +0530

    removed climbeStairs2

--- problems/ClimbingStairs.ts
+++ problems/ClimbingStairs.ts
@@ -11,17 +11,3 @@ function climbStairs(n: number, cache: { [key: number]: number } = {}): number {
 
   return cache[n];
 }
-
-function climbStairs2(n: number, cache: { [key: number]: number } = {}): number {
-  if (n <= 2 && n >= 0) {
-    return n;
-  }
-
-  if (cache[n]) {
-    return cache[n];
-  }
-
-  cache[n] = climbStairs(n - 1, cache) + climbStairs(n - 2, cache);
-
-  return cache[n];
-}
