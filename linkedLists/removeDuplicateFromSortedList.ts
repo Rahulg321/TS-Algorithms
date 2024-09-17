d3e370ebac8fb570dc3370768e82796b380d2ddc
Shivam Chopra <parthasus7973@gmail.com>
2024-09-17 23:54:01 +0530

    Solved Dupicate from linked list

---
+++ linkedLists/removeDuplicateFromSortedList.ts
@@ -0,0 +1,22 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val: number, next: ListNode | null = null) {
+      this.val = val;
+      this.next = next;
+  }
+}
+
+class Solution {
+  deleteDuplicates(head: ListNode | null): ListNode | null {
+      let temp: ListNode | null = head;
+      while (temp !== null && temp.next !== null) {
+          if (temp.val === temp.next.val) {
+              temp.next = temp.next.next;
+          } else {
+              temp = temp.next;
+          }
+      }
+      return head;
+  }
+}
